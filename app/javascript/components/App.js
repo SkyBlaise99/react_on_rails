f3ab67561d3ef420c1f984f4cacd7fca372ab30c
SkyBlaise <zsc990124@gmail.com>
2021-12-21 19:42:32 +0800

    Add react routing
    * "/" -> list of tasks
    * "/:id" -> details of task id ?

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Tasks from './Tasks/Tasks'
+import Task from './Task/Task'
 
 const App = () => {
   return (
-    <div>Hello World from App.js</div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Tasks />} />
+        <Route exact path="/:id" element={<Task />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ app/javascript/components/Task/Task.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Task = () => {
+  return (
+    <h1>Details of Task ID ?</h1>
+  )
+}
+
+export default Task
