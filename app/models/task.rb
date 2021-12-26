22175139c6178b45be2da6d022a4294abbef37dc
SkyBlaise <zsc990124@gmail.com>
2021-12-26 21:40:53 +0800

    Add validation and set is_done to false on task creation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  validates :description, presence: true
+  validates :due_date, presence: true
+
+  after_validation :set_is_done, on: [ :create ]
+
+  private
+  def set_is_done
+    self.is_done = false
+  end
 end
